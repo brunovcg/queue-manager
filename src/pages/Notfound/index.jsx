1548add5cde1358b8c941e1d86e301322146b600
brunogouveia <brunovcg@gmail.com>
2021-07-24 18:08:18 -0300

    feature pagenotFound updated

--- src/pages/Notfound/index.jsx
+++ src/pages/Notfound/index.jsx
@@ -1,8 +1,15 @@
+import { Container } from "./styles";
+import { Link } from "react-router-dom";
+import gk from '../../assets/gokitchen-neg.png'
 
 export const NotFound = () => {
-
-
-    return(
-        <div>PAGE NOT FOUND!</div>
-    )
-    }
+  return (
+    <Container>
+      <h2>PAGE NOT FOUND!</h2>
+      <figure>
+        <img src={gk} alt="gk" />
+      </figure>
+      <Link to="/">retornar a home</Link>
+    </Container>
+  );
+};
---
+++ src/pages/Notfound/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+width: 100vw;
+height: 100vh;
+background: var(--gk-green);
+
+h2{
+    color: yellow;
+
+}
+
+figure{
+    width: 30vh;
+    margin: 30px 0;
+  
+    img{
+        width:100%;
+    }
+}
+
+a{
+    color: var(--white);
+    font-size: 3vw;
+    transition: 1s;
+
+    :hover{
+        font-size: 4vw;
+    }
+
+    @media(max-width: 500px){
+        font-size: 10vw;
+    }
+}
+
+
+`
